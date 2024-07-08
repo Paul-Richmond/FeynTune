a1ab692f632ee565cff562766fb187892891b420
Paul Richmond <94543504+Paul-Richmond@users.noreply.github.com>
2024-07-08 11:07:29 +0100

    Updated python version and core/memory requirements

--- apocrita/create_env.sh
+++ apocrita/create_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -l h_rt=1:0:0
-#$ -l h_vmem=11G
-#$ -pe smp 8
+#$ -l h_vmem=7.5G
+#$ -pe smp 12
 #$ -l gpu=1
 #$ -cwd
 #$ -j y
@@ -12,12 +12,12 @@ cd ~
 module load anaconda3
 module load cuda/11.8
 
-mamba create -y -n llm python=3.8
+mamba create -y -n llm python=3.10
 mamba env update -n llm -f environment.yml
 mamba activate llm
 
 python3 -m pip install --upgrade pip
-python3 -m pip install huggingface_hub datasets wandb bitsandbytes accelerate peft python-dotenv flash-attn
+python3 -m pip install huggingface_hub datasets wandb bitsandbytes accelerate peft python-dotenv flash-attn hydra-core
 
 export BNB_CUDA_VERSION=118
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.conda/envs/llm/lib
