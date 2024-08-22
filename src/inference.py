ff7aa7ba5ecbf1e65d8d193c5e49d55371903df2
Paul Richmond <94543504+Paul-Richmond@users.noreply.github.com>
2024-08-22 13:11:24 +0100

    Moved AbstractCompleter to src/utils/callbacks

--- src/inference.py
+++ src/inference.py
@@ -8,6 +8,7 @@ from omegaconf import DictConfig, MissingMandatoryValue
 from transformers import AutoTokenizer
 import wandb
 
+from utils.callbacks import AbstractCompleter
 from utils.instantiators import load_automodelforcausallm, load_dataset_splits
 
 load_dotenv()
@@ -18,53 +19,6 @@ huggingface_hub.login(token=hf_token)
 wandb.login(key=wandb_token)
 
 logger = logging.getLogger(__name__)
-
-
-class AbstractCompleter:
-    def __init__(self, model, tokenizer, dataset, batch_size=None, generation_config=None):
-        super().__init__()
-        self.model = model.eval()
-        self.tokenizer = tokenizer
-        self.tokenizer.pad_token = tokenizer.eos_token
-        self.dataset = dataset
-
-        self.batch_size = 16 if batch_size is None else batch_size
-        if generation_config is None:
-            self.generation_config = {
-                "temperature": 0.7,
-                "top_p": 0.1,
-                "repetition_penalty": 1.18,
-                "top_k": 40,
-                "do_sample": True,
-                "max_new_tokens": 1024,
-                "pad_token_id": self.tokenizer.pad_token_id,
-                "max_time": 30
-            }
-        else:
-            self.generation_config = generation_config
-        self.label = 'abstract'
-
-    def _predict(self, batch):
-        texts = batch['prompt']
-        model_inputs = self.tokenizer(texts,
-                                      padding='longest',
-                                      truncation=True,
-                                      max_length=self.tokenizer.model_max_length,
-                                      pad_to_multiple_of=8,
-                                      add_special_tokens=False,
-                                      return_tensors="pt").to(self.model.device)
-        outputs = self.model.generate(**model_inputs, **self.generation_config)
-        batch["predictions"] = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
-        return batch
-
-    def get_predictions(self):
-        self.dataset = self.dataset.map(lambda example: {'prompt': example[self.label][:len(example[self.label]) // 2]},
-                                        desc='Generating prompts')
-        self.dataset = self.dataset.map(self._predict,
-                                        batched=True,
-                                        batch_size=self.batch_size,
-                                        desc='Generating model output')
-        return self.dataset
 
 
 @hydra.main(version_base=None, config_path="../configs", config_name="default_infer")
@@ -85,6 +39,7 @@ def main(cfg: DictConfig) -> None:
 
     ds = load_dataset_splits(cfg.dataset)
     model = load_automodelforcausallm(cfg.inference)
+    model.eval()
     tokenizer = AutoTokenizer.from_pretrained(cfg.inference.model_cfg.name, padding_side="left")
     generation_config = cfg.inference.generation_config
     batch_size = cfg.inference.batch_size
--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -1,54 +1,87 @@
 import json
 from datasets import load_dataset
-from transformers import TrainerCallback, pipeline
+from transformers import TrainerCallback, pipeline, logging
+from transformers.integrations import WandbCallback
+import torch
+
+logger = logging.get_logger(__name__)
 
 
-class GenCallback(TrainerCallback):
-    def __init__(self):
-        self.dataset = load_dataset("LLMsForHepth/arxiv_hepth_first_overfit")
-
-    def on_train_begin(self, args, state, control, model=None, tokenizer=None, **kwargs):
-        self._gen_and_save(state, model, tokenizer)
-
-    def on_save(self, args, state, control, model=None, tokenizer=None, **kwargs):
-        self._gen_and_save(state, model, tokenizer)
-
-    def _gen_and_save(self, state, model, tokenizer):
-        if (model is not None) and (tokenizer is not None):
-            generation_config = model.generation_config
-            generation_config.pad_token_id = tokenizer.eos_token_id
-
-            generator = pipeline(task="text-generation",
-                                 # do I need to unwrap the model as it a PEFT model
-                                 model=model,
-                                 tokenizer=tokenizer,
-                                 )
-
-            terminators = [
-                tokenizer.eos_token_id,
-                tokenizer.convert_tokens_to_ids("<|eot_id|>"),
-                tokenizer.convert_tokens_to_ids("<|end_of_text|>")
-            ]
-            full_dict = {}
-            for idx, abstract in enumerate(self.dataset['train']['abstract']):
-                prompt = abstract[:len(abstract) // 2]
-                # A full list of arguments that can be passed can be found in the
-                # GenerationConfig class in transformers/generation/configuration_utils.py
-                generated_text = generator(prompt,
-                                           do_sample=True,
-                                           max_length=720,
-                                           max_time=120,
-                                           eos_token_id=terminators)
-
-                paper_dict = {"Full abstract": abstract,
-                              "Prompted abstract": prompt,
-                              "Generated abstract": generated_text[0]['generated_text']
-                              }
-
-                full_dict.update({f"Paper{idx + 1}": paper_dict})
-            # TODO: this gets saved to the repo directory, change it to hydra run directory
-            json_file = f"Generated_abstracts_step_{state.global_step}.json"
-            with open(json_file, "w") as f:
-                json.dump(full_dict, f)
+class AbstractCompleter:
+    def __init__(self, model, tokenizer, dataset, batch_size=None, generation_config=None):
+        super().__init__()
+        self.model = model
+        self.tokenizer = tokenizer
+        self.dataset = dataset
+        self.batch_size = 16 if batch_size is None else batch_size
+        if generation_config is None:
+            self.generation_config = {
+                "temperature": 0.7,
+                "top_p": 0.1,
+                "repetition_penalty": 1.18,
+                "top_k": 40,
+                "do_sample": True,
+                "max_new_tokens": 1024,
+                "pad_token_id": self.tokenizer.pad_token_id,
+                "max_time": 30
+            }
         else:
-            print("Skipping generation of abstract because no model is provided")
+            self.generation_config = generation_config
+
+        self.tokenizer.pad_token = tokenizer.eos_token
+        self.original_pad_side = tokenizer.padding_side
+        self.label = 'abstract'
+
+    def _predict(self, batch):
+        texts = batch['prompt']
+        self.tokenizer.padding_side = 'left'
+        model_inputs = self.tokenizer(texts,
+                                      padding='longest',
+                                      truncation=True,
+                                      max_length=min(2048, self.tokenizer.model_max_length),
+                                      pad_to_multiple_of=8,
+                                      add_special_tokens=False,
+                                      return_tensors="pt").to(self.model.device)
+        outputs = self.model.generate(**model_inputs, **self.generation_config)
+        batch["predictions"] = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
+        self.tokenizer.padding_side = self.original_pad_side
+        return batch
+
+    def get_predictions(self):
+        self.dataset = self.dataset.map(lambda example: {'prompt': example[self.label][:len(example[self.label]) // 2]},
+                                        desc='Generating prompts')
+        self.dataset = self.dataset.map(self._predict,
+                                        batched=True,
+                                        batch_size=self.batch_size,
+                                        desc='Generating model output')
+        return self.dataset
+
+
+class GenCallback(WandbCallback):
+    def __init__(self, generation_config=None):
+        super().__init__()
+        self.generation_config = generation_config
+        self.dataset = load_dataset("LLMsForHepth/arxiv_hepth_first_overfit").get('train')
+        self.add_prompts = True
+
+    def on_evaluate(self, args, state, control, model=None, tokenizer=None, **kwargs):
+        self._gen_and_log(state, model, tokenizer)
+
+    def _gen_and_log(self, state, model, tokenizer):
+        switch_back_to_train = False
+        if model.training:
+            model.eval()
+            switch_back_to_train = True
+        with torch.no_grad():
+            completer = AbstractCompleter(model, tokenizer, dataset=self.dataset, batch_size=5,
+                                          generation_config=self.generation_config)
+            completions = completer.get_predictions()
+            new_table = self._wandb.Table(columns=['global_step', 'abstract 1', 'abstract 2',
+                                                   'abstract 3', 'abstract 4', 'abstract 5'])
+            if self.add_prompts:
+                new_table.add_data("Prompt", *completions['prompt'])
+                self.add_prompts = False
+            new_table.add_data(str(state.global_step), *completions['predictions'])
+            self._wandb.log({f"predictions": new_table}, commit=False)
+        if switch_back_to_train:
+            model.train()
