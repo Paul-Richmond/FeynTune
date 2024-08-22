3b3d1bc4f3b5430cd5bbf6ba3bafcabb36686d61
Paul Richmond <94543504+Paul-Richmond@users.noreply.github.com>
2024-08-22 13:57:40 +0100

    Removed unused code and modified trainer setup

--- src/finetune.py
+++ src/finetune.py
@@ -13,6 +13,7 @@ from utils.instantiators import (load_automodelforcausallm,
                                  instantiate_callbacks,
                                  load_dataset_splits,
                                  instantiate_training)
+from utils.metrics import compute_perplexities, metric_perplexity
 
 load_dotenv()
 hf_token = os.getenv("HUGGINGFACE_API_KEY")
@@ -20,36 +21,6 @@ wandb_token = os.getenv("WANDB_API_KEY")
 
 huggingface_hub.login(token=hf_token)
 wandb.login(key=wandb_token)
-
-
-def group_abstracts(examples, block_size=512):
-    """Concatenates the data and then divides into fixed-length chunks of size block_size."""
-    # Concatenate all texts.
-    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
-    total_length = len(concatenated_examples[list(examples.keys())[0]])
-    # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-    # customize this part to your needs.
-    if total_length >= block_size:
-        total_length = (total_length // block_size) * block_size
-    # Split by chunks of block_size.
-    result = {
-        k: [t[i: i + block_size] for i in range(0, total_length, block_size)]
-        for k, t in concatenated_examples.items()
-    }
-    return result
-
-
-# see https://github.com/huggingface/transformers/issues/28066
-class CustomCollator:
-    def __init__(self, tokenizer):
-        self.tokenizer = tokenizer
-
-    def __call__(self, features):
-        # features is a python list
-        features = [{k: v for k, v in d.items() if k != 'labels'} for d in features]
-        batch = self.tokenizer.pad(features, return_tensors="pt")
-        batch["labels"] = batch["input_ids"]
-        return batch
 
 
 @hydra.main(version_base=None, config_path="../configs", config_name="default")
@@ -61,33 +32,27 @@ def main(cfg: DictConfig) -> None:
                           batched=True,
                           remove_columns=ds['train'].column_names,
                           desc="Tokenizing")
-    if cfg.tokenizer.post_processing.get('add_labels'):
-        tokenised_ds = tokenised_ds.map(lambda example: {"labels": example["input_ids"]},
-                                        batched=True,
-                                        desc="Adding labels")
-    if cfg.tokenizer.post_processing.get('concatenate_and_split_length') is not None:
-        tokenised_ds = tokenised_ds.map(group_abstracts,
-                                        fn_kwargs={'block_size': cfg.tokenizer.post_processing.concatenate_and_split_length},
-                                        batched=True,
-                                        desc="Applying group abstracts")
+    tokenised_ds = tokenised_ds.map(lambda example: {"labels": example["input_ids"]},
+                                    batched=True,
+                                    desc="Adding labels")
 
     model = load_automodelforcausallm(cfg.model)
     trainer_cls, training_args = instantiate_training(cfg.training)
     # why pad_to_multiple_of=8? see https://docs.nvidia.com/deeplearning/performance/mixed-precision-training/index.html#opt-tensor-cores
-    data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, pad_to_multiple_of=8)  #dynamically pads a batch to all have same tensor shapes
+    data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, pad_to_multiple_of=8)  # dynamically pads a batch to all have same tensor shapes
     callbacks = instantiate_callbacks(cfg.callbacks)
     optimizer = load_optimizer(cfg.optimizer, model)
 
     trainer = trainer_cls(model=model,
                           args=training_args,
                           train_dataset=tokenised_ds.get('train'),
-                          eval_dataset=tokenised_ds.get('test', None),
+                          eval_dataset=tokenised_ds,  # will evaluate all datasets within tokenised_ds
                           data_collator=data_collator,
                           tokenizer=tokenizer,
                           optimizers=(optimizer, None),
                           callbacks=callbacks,
-                          compute_metrics=None,
-                          preprocess_logits_for_metrics=None
+                          compute_metrics=metric_perplexity,
+                          preprocess_logits_for_metrics=compute_perplexities
                           )
 
     trainer.train(resume_from_checkpoint=cfg.training.resume_from_checkpoint)
