90948b767b887b52b76cb937a6c4ba8f199ef4f9
Paul Richmond <94543504+Paul-Richmond@users.noreply.github.com>
2024-11-21 11:52:26 +0000

    Changes to infer_hf.py

--- src/infer_hf.py
+++ src/infer_hf.py
@@ -10,21 +10,24 @@ import torch
 from torch.utils.data import DataLoader
 from transformers import (AutoTokenizer,
                           AutoModelForCausalLM,
-                          DataCollatorForSeq2Seq)
+                          DataCollatorForSeq2Seq,
+                          set_seed)
 
 from utils.io import load_dataset_splits, save_dict_to_json
 from utils.processing import split_abstracts
 
 load_dotenv()
 hf_token = os.getenv("HUGGINGFACE_API_KEY")
-wandb_token = os.getenv("WANDB_API_KEY")
+# wandb_token = os.getenv("WANDB_API_KEY")
 
 huggingface_hub.login(token=hf_token)
-wandb.login(key=wandb_token)
+# wandb.login(key=wandb_token)
 
 logger = logging.getLogger(__name__)
 logging.basicConfig()
 logger.setLevel(logging.INFO)
+
+set_seed(42)
 
 gpu_memory = torch.cuda.get_device_properties(0).total_memory / (1024 ** 3)  # in Gb
 initial_batch_size = 192
@@ -49,6 +52,10 @@ dataloader_params = {"batch_size": batch_size,
                      }
 
 
+def parse_completions(abstracts, completions):
+    return [completion[len(abstract):] for abstract, completion in zip(abstracts, completions)]
+
+
 @hydra.main(version_base=None, config_path="../configs", config_name="default_infer")
 def main(cfg: DictConfig) -> None:
     ds = load_dataset_splits(cfg.dataset)  # expect DatasetDict object with a single key
@@ -71,12 +78,12 @@ def main(cfg: DictConfig) -> None:
                                     batched=True,
                                     desc="Adding labels")
 
-    tokenised_ds = tokenised_ds.map(lambda example: {"token_num": len(example["input_ids"])},
-                                    batched=False,
-                                    desc="Getting token counts")
-    tokenised_ds = tokenised_ds.sort(column_names='token_num', reverse=True)
-    sorted_ids = tokenised_ds['id']
-    sorted_prompts = tokenised_ds['prompt']
+    # tokenised_ds = tokenised_ds.map(lambda example: {"token_num": len(example["input_ids"])},
+    #                                 batched=False,
+    #                                 desc="Getting token counts")
+    # tokenised_ds = tokenised_ds.sort(column_names='token_num', reverse=True)
+    # sorted_ids = tokenised_ds['id']
+    # sorted_prompts = tokenised_ds['prompt']
     tokenised_ds = tokenised_ds.remove_columns(list(set(tokenised_ds.column_names)
                                                     - {'input_ids', 'attention_mask', 'labels'}))
 
@@ -90,34 +97,43 @@ def main(cfg: DictConfig) -> None:
 
     generation_cfg = OmegaConf.to_container(cfg.inference.generation_cfg, resolve=True)
     generation_cfg['pad_token_id'] = tokenizer.pad_token_id
-    output_dict = {'model': cfg.inference.model_name.split('/')[-1],
-                   'dataset': {'name': cfg.dataset.name, 'splits': ds_split_names[0]},
-                   'ids': sorted_ids,
-                   'prompts': sorted_prompts,
-                   'generation_config': generation_cfg,
-                   }
+    # output_dict = {'model': cfg.inference.model_name.split('/')[-1],
+    #                'dataset': {'name': cfg.dataset.name, 'splits': ds_split_names[0]},
+    #                'ids': sorted_ids,
+    #                'prompts': sorted_prompts,
+    #                'generation_config': generation_cfg,
+    #                }
 
-    predictions = []
+    completions = []
     model.eval()
+    logger.info("--------------------------")
+    logger.info(f"Generating using model {cfg.inference.model_name}")
+    logger.info("--------------------------")
     for idx, batch in enumerate(dataloader):
         logger.info(f"Generating batch {idx}")
         with torch.inference_mode():
             batch.to(model.device)
             outputs_tok = model.generate(**batch, **generation_cfg)
             batch_predictions = tokenizer.batch_decode(outputs_tok, skip_special_tokens=True)
-            predictions.extend(batch_predictions)
-            output_dict['predictions'] = predictions
-            output_dict['batch'] = idx
+            completions.extend(batch_predictions)
+            # output_dict['predictions'] = predictions
+            # output_dict['batch'] = idx
 
-        if idx % 10 == 0:  # save every 10 batches
-            save_dict_to_json(data=output_dict,
-                              directory=cfg.inference.output_dir,
-                              filename=cfg.inference.output_fname)
+    #     if idx % 10 == 0:  # save every 10 batches
+    #         save_dict_to_json(data=output_dict,
+    #                           directory=cfg.inference.output_dir,
+    #                           filename=cfg.inference.output_fname)
+    #
+    # output_dict['batch'] = 'end of inference'
+    # save_dict_to_json(data=output_dict,
+    #                   directory=cfg.inference.output_dir,
+    #                   filename=cfg.inference.output_fname)
+    y_pred = parse_completions(ds['prompt'], completions)
 
-    output_dict['batch'] = 'end of inference'
-    save_dict_to_json(data=output_dict,
-                      directory=cfg.inference.output_dir,
-                      filename=cfg.inference.output_fname)
+    ds = ds.add_column(name=f"comp_{cfg.inference.model_name.split('/')[-1]}", column=completions)
+    ds = ds.add_column(name=f"preds_{cfg.inference.model_name.split('/')[-1]}", column=y_pred)
+    ds.push_to_hub(cfg.inference.repo_name, split='test')
+    huggingface_hub.logout()
 
 
 if __name__ == '__main__':
